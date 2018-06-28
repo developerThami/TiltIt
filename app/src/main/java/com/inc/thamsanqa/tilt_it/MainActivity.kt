3d042a2e5fc89cf817fcfe6f794b1eb89baacdb5
Thamsanqa Mahlaola <212426652thami@gmail.com>
2018-06-28 16:08:00 +0200

    --added random tilt direction generator.

--- app/src/main/java/com/inc/thamsanqa/tilt_it/MainActivity.kt
+++ app/src/main/java/com/inc/thamsanqa/tilt_it/MainActivity.kt
@@ -9,6 +9,7 @@ import android.hardware.SensorManager
 import android.os.CountDownTimer
 import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 
 class MainActivity : AppCompatActivity(), SensorEventListener {
@@ -18,7 +19,20 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
 
     private var mGravity: FloatArray? = null
 
-    var time:Long = 0
+    enum class Direction {
+        UP,
+        DOWN,
+        LEFT,
+        RIGHT,
+        NONE
+    }
+
+    lateinit var tiltDirection: Direction
+
+    lateinit var expectedTiltDirection: Direction
+    lateinit var actualtiltDirection: Direction
+
+    var time: Long = 0
 
     var orientation = FloatArray(3)
     private var pitch: Float = 0.0f
@@ -31,7 +45,8 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
         mSensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
         accelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
 
-        //initListeners()
+        initListeners()
+        tiltDirection = Direction.NONE
 
         //timer.start()
     }
@@ -45,20 +60,56 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
 
     override fun onSensorChanged(event: SensorEvent) {
 
-
         if (event.sensor.type == Sensor.TYPE_ACCELEROMETER) {
+
             mGravity = event.values
 
             val x = event.values[0].toDouble()
             val y = event.values[1].toDouble()
             val z = event.values[2].toDouble()
 
-            roll = (Math.atan2(y, z) * 57.3).toFloat()
-            pitch = (Math.atan2((-x), Math.sqrt(y * y + z * z)) * 57.3).toFloat()
+            tiltDirection = determineTiltDirection(x, y)
+            roll = determineRoll(y, z)
+            pitch = determinePitch(x, y, z)
 
-            Log.d("Angles", "X:$pitch , Y:$roll")
+
+            Log.d("Angles", "X:$pitch , Y:$roll , Direction:$tiltDirection")
         }
 
+    }
+
+    private fun determinePitch(x: Double, y: Double, z: Double): Float {
+        return (Math.atan2((-x), Math.sqrt(y * y + z * z)) * 57.3).toFloat()
+    }
+
+    private fun determineRoll(y: Double, z: Double): Float {
+        return (Math.atan2(y, z) * 57.3).toFloat()
+    }
+
+    private fun determineTiltDirection(x: Double, y: Double): Direction {
+
+        if (Math.abs(x) > Math.abs(y)) {
+
+            if (x < 0)
+                return Direction.UP
+
+            if (x > 0)
+                return Direction.DOWN
+
+        } else {
+
+            if (y < 0)
+                return Direction.LEFT
+
+            if (y > 0)
+                return Direction.RIGHT
+        }
+
+        if (x > -2 && x < 2 && y > -2 && y < 2) {
+            return Direction.NONE
+        }
+
+        return Direction.NONE
     }
 
     public override fun onDestroy() {
@@ -85,7 +136,7 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
     var totalMilliSeconds: Long = 1000
     var intervalMilliSeconds: Long = 1
 
-    private var timer: CountDownTimer = object : CountDownTimer( totalMilliSeconds, intervalMilliSeconds) {
+    private var timer: CountDownTimer = object : CountDownTimer(totalMilliSeconds, intervalMilliSeconds) {
         override fun onTick(millisUntilFinished: Long) {
             totalMilliSeconds = millisUntilFinished
         }
@@ -97,5 +148,19 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
 
     private fun startGame() {
 
+    }
+
+    private fun randomDirectionGenerator(): Direction {
+
+        val randomNumber = Random().nextInt(4) + 1
+
+        when (randomNumber) {
+            0 -> return Direction.DOWN
+            1 -> return Direction.DOWN
+            2 -> return Direction.LEFT
+            3 -> return Direction.RIGHT
+        }
+
+        return Direction.NONE
     }
 }
